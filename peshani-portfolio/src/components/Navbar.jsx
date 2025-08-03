39dddb2d49c2813da35e10d995422e3481f07f9d
Peshani-Rasangika <prranaweeraos2001@gmail.com>
2025-08-03 08:46:37 +0530

    Refactor Navbar component for improved mobile navigation and styling

--- peshani-portfolio/src/components/Navbar.jsx
+++ peshani-portfolio/src/components/Navbar.jsx
@@ -4,20 +4,39 @@ import "./Navbar.css";
 import logo from "../assets/logo.svg";
 
 function Navbar() {
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-logo-link">
+      <Link to="/" className="nav-logo-link" onClick={closeNav}>
         <img src={logo} alt="PRR" className="nav-logo" />
       </Link>
-      <ul className="nav-links">
+      <div className="menu-icon" onClick={toggleNav}>
+        <div className={isNavOpen ? "hamburger open" : "hamburger"}></div>
+      </div>
+      <ul className={isNavOpen ? "nav-links open" : "nav-links"}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeNav}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={closeNav}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" onClick={closeNav}>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
